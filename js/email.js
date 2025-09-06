d6693134449467784f2bf44c5b537294d81621dc
Brian Teddy <teddybrian543@gmail.com>
2025-09-06 14:16:07 +0300

    Add email contact function

--- js/email.js
+++ js/email.js
@@ -1,7 +1,7 @@
 document.getElementById("contact-form").addEventListener("submit", function(event) {
   event.preventDefault();
 
-  emailjs.sendForm("", "", this)
+  emailjs.sendForm("service_4w817f9", "template_x1jnxbo", this)
     .then(() => {
       alert("Message sent successfully!");
     }, (error) => {
